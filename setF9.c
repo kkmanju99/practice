38e3be30943939132b9418e42168a8b4eea7e773
kkmanju99 <36377358+kkmanju99@users.noreply.github.com>
2019-01-20 13:24:16 +0530

    Create setF9.c

---
+++ setF9.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,k,a[10],i,sum=0;
+clrscr();
+scanf("%d%d",&n,&k);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a);
+}
+for(i=0;i<=k;i++)
+{
+sum=sum+i;
+}
+printf("%d",sum);
+getch();
+}
