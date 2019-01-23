556198478e0b8a3c29977446eb28d3dc19aa8ad2
kkmanju99 <36377358+kkmanju99@users.noreply.github.com>
2019-01-23 15:50:29 +0530

    Create playset2.c

---
+++ playset2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,fact=1,i;
+    scanf("%d",&n);
+    if(n<=20)
+    {
+        for(i=1;i<=n;i++)
+        {
+            fact=fact*i;
+        }
+        printf("%d",fact);
+    }
+    else
+    {
+        printf("not in range");
+    }
+
+    return 0;
+}
