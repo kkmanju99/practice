399c03630d1d65f33132f2d44bf71a2bc291d5bd
kkmanju99 <36377358+kkmanju99@users.noreply.github.com>
2019-01-23 21:39:43 +0530

    Update setT10.c

--- setT10.c
+++ setT10.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-#include<conio.h>
 void main()
 {
 int m1,m2,h1,h2,ans1,ans2;
@@ -7,6 +6,11 @@ scanf("%d %d",&h1,&m1);
 scanf("%d %d",&h2,&m2);
 ans1=h1-h2;
 ans2=m1-m2;
+if(ans1<0||ans2<0)
+{
+    ans1=(ans1*ans1)/(-ans1);
+    ans2=(ans2*ans2)/(-ans2);
+}
 printf("%d %d",ans1,ans2);
-getch();
+
 }
