d55a7e62cb2ea71b2055aa16fd817a4993e1769b
kkmanju99 <36377358+kkmanju99@users.noreply.github.com>
2019-02-09 18:38:02 +0530

    Create setSIX3.c

---
+++ setSIX3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, sum = 0,rem;
+
+    /* Input number from user */
+    scanf("%d", &n);
+    while(n>0)
+    {
+        rem=n%10;
+        sum=sum+rem;
+        n=n/10;
+    }
+        printf("%d",sum);
+
+    return 0;
+}
