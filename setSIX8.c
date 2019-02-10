ef250cddce6123d742e54dcc8f7ddcae7e279cc1
kkmanju99 <36377358+kkmanju99@users.noreply.github.com>
2019-02-10 20:18:23 +0530

    Create setSIX8.c

---
+++ setSIX8.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[100],n,k,m=0,i;
+    scanf("%d%d",&n,&k);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=n-1;i>=0;i--)
+    {
+        if(a[i]==k)
+        {
+            m++;
+    }
+    }
+    if(m!=0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    
+    return 0;
+}
