842e78f391cda494209d2a3ba0db65663cb372e6
kkmanju99 <36377358+kkmanju99@users.noreply.github.com>
2019-02-11 12:30:36 +0530

    Create setEIG8.c

---
+++ setEIG8.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() 
+{
+    int n1,i;
+    scanf("%d",&n1);
+    if(n1%13==0)
+    {
+        printf("yes");
+    }
+    else
+    {
+    printf("no");
+    }
+	return 0;
+}
