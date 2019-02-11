e0e2bfe08e32482f97376970629f171bc2bef684
kkmanju99 <36377358+kkmanju99@users.noreply.github.com>
2019-02-11 13:29:22 +0530

    Create setNINE8.c

---
+++ setNINE8.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) 
+{
+int x,y,remainder,lcm,gcd,a,b;
+scanf("%d",&x);
+scanf("%d",&y);
+a=x;
+b=y;
+do
+{
+remainder=x%y;
+if(remainder==0)
+break;
+x=y;
+y=remainder;
+}while(remainder!=0);
+gcd=y;
+lcm=(a*b)/gcd;
+printf("\n %d",lcm);
+return 0;
+}
