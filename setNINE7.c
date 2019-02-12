91011e73004c852cd39763e77c31b52153e7e608
kkmanju99 <36377358+kkmanju99@users.noreply.github.com>
2019-02-12 10:38:00 +0530

    Create setNINE7.c

---
+++ setNINE7.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+
+
+int main(void) 
+
+{
+
+int x,y,remainder,gcd;
+
+
+scanf("%d",&x);
+
+
+scanf("%d",&y);
+
+do
+
+{
+
+remainder=x%y;
+
+if(remainder==0)
+
+break;
+
+x=y;
+
+y=remainder;
+
+}while(remainder!=0);
+
+gcd=y;
+
+printf("\n%d",gcd);
+
+return 0;
+
+}
