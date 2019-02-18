b8588089ec2e62e41df440b6c65c8ba7b55cd575
kkmanju99 <36377358+kkmanju99@users.noreply.github.com>
2019-02-18 21:20:46 +0530

    Create playset27.c

---
+++ playset27.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) {
+  char a[1000];
+  int i;
+scanf("%s",a);
+for(i=0;a[i]!='\0';i++)
+{
+  if(a[i]>=65&&a[i]<=90)
+  {
+    a[i]=a[i]+32;
+  }
+  else if(a[i]>=97&&a[i]<=122)
+  {
+    a[i]=a[i]-32;
+  }
+}
+for(i=0;a[i]!='\0';i++)
+{
+  printf("%c",a[i]);
+}
+  return 0;
+}
