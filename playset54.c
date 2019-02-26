71e3c52a0e5e3aa20138b038b055c342583ecabf
kkmanju99 <36377358+kkmanju99@users.noreply.github.com>
2019-02-26 22:04:36 +0530

    Create playset54.c

---
+++ playset54.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main(void) {
+  char a[1000],b[1000];
+  int c=0,i,j,d=0,e=0;
+  scanf("%s%s",a,b);
+   for(i=0;a[i]!='\0';i++)
+  {
+    d++; 
+  }
+    for(j=0;b[j]!='\0';j++)
+    {
+      e++; 
+    }
+    if(d==e)
+    {
+  for(i=0;a[i]!='\0';i++)
+  { 
+    for(j=0;b[j]!='\0';j++)
+    { 
+if(a[i]==b[j])
+{
+  c++;
+}
+    }
+  }
+  if(c==d)
+  {
+    printf("yes");
+  }
+  else
+  {
+    printf("no");
+  }
+    }
+    else
+    {
+      printf("no");
+    }
+
+
+  return 0;
+}
