d2f15f545aa351265a447110b7ef1b1896b7548e
kkmanju99 <36377358+kkmanju99@users.noreply.github.com>
2019-02-27 21:13:46 +0530

    Create playset70.c

---
+++ playset70.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+char a[100000],b[10000]="Answer";
+int i,c=0;
+scanf("%s",a);
+for(i=0;a[i]!='\0';i++)
+{
+  c++;
+}
+for(i=0;i<=6;i++)
+{
+  a[c]=b[i];
+  c++;
+}
+printf("%s",a);
+  
+return 0;
+}
