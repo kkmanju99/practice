8d87679bd6f31086a12d755099680eda822a6e4e
kkmanju99 <36377358+kkmanju99@users.noreply.github.com>
2019-03-11 20:13:43 +0530

    Create playset81.c

---
+++ playset81.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+  int n,i,a[100],s,m=0,j;
+  scanf("%d",&n);
+  
+  for(i=0;i<n;i++)
+  {
+      scanf("%d",&a[i]);
+  }
+
+  for(i=0;i<=n;i++)
+  {
+   for(j=i;j<n;j++)
+		{
+			s=a[i]&a[j];
+			if(m<s)
+			{
+				m=s;
+			}
+		}
+		
+}
+
+  printf("%d",m);
+  
+    return 0;
+}
