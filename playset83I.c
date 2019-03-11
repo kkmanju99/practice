fe8e905ccc631e4290e2390fe25629c36b7ef1be
kkmanju99 <36377358+kkmanju99@users.noreply.github.com>
2019-03-11 20:45:11 +0530

    Create playset83I.c

---
+++ playset83I.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+  int n,i,a[100],s;
+  scanf("%d",&n);
+  
+  for(i=0;i<n;i++)
+  {
+    scanf("%d",&a[i]);
+  }
+ s=a[0];
+  for(i=1;i<n;i++)
+  {
+   
+   s=s|a[i];
+  }
+
+  printf("%d",s);
+  
+    return 0;
+}
