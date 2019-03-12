e3d406661135b7fad42de8989c9311530caf0ca4
kkmanju99 <36377358+kkmanju99@users.noreply.github.com>
2019-03-12 15:58:50 +0530

    Create playset86.c

---
+++ playset86.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+  int n,i,a[100],s,m=0,j;
+  scanf("%d",&n);
+  
+  for(i=0;i<n;i++)
+  {
+      scanf("%d",&a[i]);
+  }
+  s=a[0];
+
+  for(i=1;i<=n;i++)
+  {
+  
+			s=s^a[i];
+  }
+
+  printf("%d",s);
+  
+    return 0;
+}
