72203522eb6b3ee83b7677060493bdfd1ecdccd2
kkmanju99 <36377358+kkmanju99@users.noreply.github.com>
2019-03-14 21:07:08 +0530

    Create playset100.c

---
+++ playset100.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+	long int n,r1=0,i=1,sum=0;
+	scanf("%ld",&n);
+	while(n!=0)
+	{
+		r1=n%10;
+		sum=sum+r1*i;
+		n=n/10;
+		i=i*2;
+	}
+	printf("%lx",sum);
+	return 0;
+}
