0516f5d6de662ff704c372a5ef8da6685536904b
kkmanju99 <36377358+kkmanju99@users.noreply.github.com>
2019-03-24 20:12:25 +0530

    Create playset165.c

---
+++ playset165.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+{
+    int a[100],i,n,k,f=0,c;
+    scanf("%d%d",&n,&k);
+    for(i=0;i<n;i++)
+       scanf("%d",&a[i]);
+   
+    c=a[0];
+    for(i=0;i<n;i++)
+    {
+    
+         if((a[i]>c)&&(a[i]>k))
+         {
+             printf("%d",a[i]);
+             break;
+         }
+         else
+           c=a[i];
+         
+    }
+    
+
+}
