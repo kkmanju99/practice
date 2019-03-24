1eef598d3b43fe1b11773d94423371eae72a0514
kkmanju99 <36377358+kkmanju99@users.noreply.github.com>
2019-03-24 10:52:33 +0530

    Create playset160.c

---
+++ playset160.c
@@ -0,0 +1,19 @@
+#include<stdio.h>    
+void main()
+{  
+int a[10],n,i,c,b,count1=0;    
+scanf("%d %d",&c,&b);  
+n=c|b;
+for(i=0;n>0;i++)    
+{    
+a[i]=n%2;    
+n=n/2;    
+}    
+for(i=i-1;i>=0;i--)    
+{    
+
+if(a[i]==1)
+count1++;
+} 
+printf("%d",count1);
+}  
